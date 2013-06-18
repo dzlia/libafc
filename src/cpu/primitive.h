2c4820067fd2de00c2a96e340ef153eac923f94a
Dźmitry Laŭčuk <>
2013-06-18 20:59:40 +0300

    1) manipulations with single bytes are made simple;2) working constructors are added that take unsigned char[] and endianness as input

--- src/cpu/primitive.h
+++ src/cpu/primitive.h
@@ -27,9 +27,10 @@ namespace afc
 		operator T() const {return static_cast<T>(m_data.dword);}
 
 		template<endianness dest> inline void toBytes(unsigned char out[]) const;
-		template<endianness src> inline static D fromBytes(const unsigned char in[]);
+		template<endianness src> inline static D fromBytes(const unsigned char in[]) {return D(in, src);}
 	protected:
-		Int32Base(const T i, const endianness interpretation) {set(i, interpretation);}
+		Int32Base(const T i, const endianness byteOrder) {set(i, byteOrder);}
+		Int32Base(const unsigned char in[], const endianness byteOrder = PLATFORM_BYTE_ORDER);
 		template<endianness bo> Int32Base(const Int32Base<T, D, bo> &i) {set(i.data.dword, bo);}
 
 		static_assert(sizeof(T) == 4, "T is not a dword");
@@ -41,33 +42,52 @@ namespace afc
 	private:
 		data m_data;
 
-		inline void set(const T i, const endianness interpretation);
-		template<endianness dest> inline void setBytes(unsigned char out[]) const;
+		inline void set(const T i, const endianness byteOrder);
 	};
 
 	template<endianness o = PLATFORM_BYTE_ORDER> struct Int32 : public Int32Base<int32_t, Int32<o>, o>
 	{
-		Int32(const int32_t i, const endianness interpretation = PLATFORM_BYTE_ORDER) : Int32Base<int32_t, Int32<o>, o>(i, interpretation) {}
+		Int32(const int32_t i, const endianness byteOrder = PLATFORM_BYTE_ORDER)
+				: Int32Base<int32_t, Int32<o>, o>(i, byteOrder) {}
+		Int32(const unsigned char in[], const endianness byteOrder = PLATFORM_BYTE_ORDER)
+				: Int32Base<int32_t, Int32<o>, o>(in, byteOrder) {}
 		template<endianness bo> Int32(const Int32<bo> &i) : Int32Base<int32_t, Int32<o>, o>(i) {}
-		template<endianness src> Int32(const unsigned char in[]) : Int32Base<int32_t, Int32<o>, o>(in) {}
 
 		template<endianness dest> operator Int32<dest>() const;
 	};
 
 	template<endianness o = PLATFORM_BYTE_ORDER> struct UInt32 : public Int32Base<uint32_t, UInt32<o>, o>
 	{
-		UInt32(const uint32_t i, const endianness interpretation = PLATFORM_BYTE_ORDER) : Int32Base<uint32_t, UInt32<o>, o>(i, interpretation) {}
+		UInt32(const uint32_t i, const endianness byteOrder = PLATFORM_BYTE_ORDER)
+				: Int32Base<uint32_t, UInt32<o>, o>(i, byteOrder) {}
+		UInt32(const unsigned char in[], const endianness byteOrder = PLATFORM_BYTE_ORDER)
+				: Int32Base<int32_t, Int32<o>, o>(in, byteOrder) {}
 		template<endianness bo> UInt32(const UInt32<bo> &i) : Int32Base<uint32_t, UInt32<o>, o>(i) {}
-		template<endianness src> UInt32(const unsigned char in[]) : Int32Base<uint32_t, UInt32<o>, o>(in) {};
 
 		template<endianness dest> operator UInt32<dest>() const;
 	};
 }
 
 template <typename T, typename D, afc::endianness o>
-inline void afc::Int32Base<T, D, o>::set(const T i, const afc::endianness interpretation)
+inline afc::Int32Base<T, D, o>::Int32Base(const unsigned char in[], const afc::endianness byteOrder)
 {
-	if (interpretation == o) {
+	if (byteOrder == o) {
+		m_data.bytes[0] = in[0];
+		m_data.bytes[1] = in[1];
+		m_data.bytes[2] = in[2];
+		m_data.bytes[3] = in[3];
+	} else {
+		m_data.bytes[0] = in[3];
+		m_data.bytes[1] = in[2];
+		m_data.bytes[2] = in[1];
+		m_data.bytes[3] = in[0];
+	}
+}
+
+template <typename T, typename D, afc::endianness o>
+inline void afc::Int32Base<T, D, o>::set(const T i, const afc::endianness byteOrder)
+{
+	if (byteOrder == o) {
 		m_data.dword = i;
 	} else {
 		data input = {i};
@@ -92,15 +112,6 @@ inline void afc::Int32Base<T, D, src>::toBytes(unsigned char out[]) const
 		out[2] = m_data.bytes[1];
 		out[3] = m_data.bytes[0];
 	}
-}
-
-template <typename T, typename D, afc::endianness o> template <afc::endianness src>
-inline D afc::Int32Base<T, D, o>::fromBytes(const unsigned char in[])
-{
-	const uint32_t val = (src==endianness::BE)^(o==endianness::BE)^(PLATFORM_BYTE_ORDER==endianness::BE) ?
-			(((((in[0]<<8) + in[1])<<8) + in[2])<<8) + in[3] :
-			(((((in[3]<<8) + in[2])<<8) + in[1])<<8) + in[0];
-	return D(val, o);
 }
 
 #endif /*AFC_CPU_PRIMITIVE_H_*/
