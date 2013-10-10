cf132ebc334bacb902bb2b99793e1e0c0dbd2668
Dźmitry Laŭčuk <>
2013-10-10 13:49:52 +0300

    The functions are removed that are already provided by the standard library; some functions are marked with 'constexpr'.

--- src/afc/math_utils.h
+++ src/afc/math_utils.h
@@ -9,33 +9,26 @@ namespace afc
 {
 	const unsigned INT_SIGN_BIT_MASK = ~0 ^ INT_MAX;
 
-	inline int abs(const int i) throw()
+	inline constexpr bool sameSign(const int i, const int j) throw()
 	{
-		return i >= 0 ? i : -i;
-	}
-
-	inline bool sameSign(const int i, const int j) throw()
-	{
-		// TODO could be better
-		if (i == 0 || j == 0) {
-			return (i ^ j) == 0;
-		}
-		return (i & INT_SIGN_BIT_MASK) ==  (j & INT_SIGN_BIT_MASK);
+		// TODO (i == 0 || j == 0) ? (i ^ j) == 0 could be better
+		return (i == 0 || j == 0) ? (i ^ j) == 0 :
+				(i & INT_SIGN_BIT_MASK) ==  (j & INT_SIGN_BIT_MASK);
 	}
 
 	const int PARITY_MASK = 0x1;
 
-	inline bool isEven(const int i) throw()
+	inline constexpr bool isEven(const int i) throw()
 	{
 		return (i & PARITY_MASK) == 0;
 	}
 
-	inline bool isOdd(const int i) throw()
+	inline constexpr bool isOdd(const int i) throw()
 	{
 		return (i & PARITY_MASK) != 0;
 	}
 
-	template<typename T> inline T mean(const T x, const T y) noexcept
+	template<typename T> inline constexpr T mean(const T x, const T y) noexcept
 	{
 		static_assert(std::is_integral<T>::value, "T must be an integral type.");
 		return (x>>1) + (y>>1) + (x&y&1);
@@ -45,15 +38,6 @@ namespace afc
 	inline const int intSqrt(const int x) throw()
 	{
 		return (int) floor(sqrt(x)+0.5);
-	}
-
-	inline const double round(const double val) throw()
-	{
-	#ifdef __GNUC__
-		return ::round(val);
-	#else
-		return floor(val + 0.5);
-	#endif
 	}
 }
 
