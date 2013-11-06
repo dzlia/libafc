bdbecb1a2a4c0016234f5eb99dfa303d1c677648
Dźmitry Laŭčuk <>
2013-11-06 23:30:10 +0300

    Some variable names are fixed.

--- src/afc/utils.h
+++ src/afc/utils.h
@@ -29,8 +29,8 @@ namespace afc
 
 	std::string convertToUtf8(const char *src, const char *encoding);
 	std::string convertToUtf8(const std::string &src, const char *encoding);
-	std::string convertFromUtf8(const char *stc, const char *encoding);
-	std::string convertFromUtf8(const std::string &stc, const char *encoding);
+	std::string convertFromUtf8(const char *src, const char *encoding);
+	std::string convertFromUtf8(const std::string &src, const char *encoding);
 
 	// code points have platform endianness, while characters are little-endian
 	std::u16string stringToUTF16LE(const char * const src, const char * const encoding);
