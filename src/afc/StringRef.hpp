bf842d10d9a2da7ef33b26846db493c2108f3bb3
Dźmitry Laŭčuk <>
2014-06-25 14:28:37 +0300

    afc::StringRef is introduced; common functionality between ConstStringRef and StringRef is moved to the template afc::StringRefBase.

--- src/afc/StringRef.hpp
+++ src/afc/StringRef.hpp
@@ -13,39 +13,52 @@ GNU Lesser General Public License for more details.
 
 You should have received a copy of the GNU Lesser General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>. */
-#ifndef AFC_CONSTSTRINGREF_HPP_
-#define AFC_CONSTSTRINGREF_HPP_
+#ifndef AFC_STRINGREF_HPP_
+#define AFC_STRINGREF_HPP_
 
 #include <cstddef>
+#include <type_traits>
 
 namespace afc
 {
 	// Allows for efficient processing of string literals by resolving their size at compile time.
-	class ConstStringRef
+	template<typename CharType>
+	class StringRefBase
 	{
-	public:
-		ConstStringRef(const ConstStringRef &) = default;
-		ConstStringRef(ConstStringRef &&) = default;
-		ConstStringRef &operator=(const ConstStringRef &) = default;
-		ConstStringRef &operator=(ConstStringRef &&) = default;
-		~ConstStringRef() = default;
-
-		template <std::size_t n>
-		ConstStringRef(const char (&str)[n]) : m_str(str), m_size(n - 1) {}
-
-		explicit operator const char *() const { return m_str; }
-
-		const char *value() { return m_str; }
-		const size_t size() const { return m_size; }
-
-		char operator[](size_t i) const { return m_str[i]; };
-
-		const char *begin() const { return &m_str[0]; };
-		const char *end() const { return &m_str[m_size]; };
 	private:
-		const char *m_str;
+		// Facilitates type casting from StringRefBase<CharType> to StringRefBase<const CharType>.
+		friend class StringRefBase<typename std::remove_const<CharType>::type>;
+		StringRefBase(CharType * const str, const size_t size) : m_str(str), m_size(size) {}
+	public:
+		StringRefBase(const StringRefBase &) = default;
+		StringRefBase(StringRefBase &&) = default;
+		StringRefBase &operator=(const StringRefBase &) = default;
+		StringRefBase &operator=(StringRefBase &&) = default;
+		~StringRefBase() = default;
+
+		template<std::size_t n>
+		StringRefBase(CharType (&str)[n]) : m_str(str), m_size(n - 1) {}
+
+		operator StringRefBase<const CharType>() { return StringRefBase<const CharType>(m_str, m_size); }
+
+		explicit operator CharType *() { return m_str; }
+		explicit operator const CharType *() const { return m_str; }
+
+		CharType *value() { return m_str; }
+		const CharType *value() const { return m_str; }
+		size_t size() const { return m_size; }
+
+		CharType operator[](size_t i) const { return m_str[i]; };
+
+		CharType *begin() const { return &m_str[0]; };
+		CharType *end() const { return &m_str[m_size]; };
+	private:
+		CharType *m_str;
 		const size_t m_size;
 	};
+
+	typedef StringRefBase<char> StringRef;
+	typedef StringRefBase<const char> ConstStringRef;
 }
 
-#endif /* AFC_CONSTSTRINGREF_HPP_ */
+#endif /* AFC_STRINGREF_HPP_ */
