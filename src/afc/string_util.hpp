5c5f0ddd387d69d1eca0958571a767c06b2dad3d
Dźmitry Laŭčuk <>
2014-07-02 17:35:34 +0300

    1) afc::Tokeniser::next(CharType *&, CharType *&) is introduced to be used when copying is not needed; 2) afc::Tokeniser::next(void): code compiles if C++ exceptions are disabled.

--- src/afc/string_util.hpp
+++ src/afc/string_util.hpp
@@ -32,29 +32,33 @@ namespace afc
 		Tokeniser &operator=(const Tokeniser &) = delete;
 		Tokeniser &operator=(Tokeniser &&) = delete;
 	public:
-		Tokeniser(const std::basic_string<CharType> &str, const CharType delimiter)
+		Tokeniser(std::basic_string<CharType> &str, const CharType delimiter)
 			: m_str(str), m_delimiter(delimiter), m_begin(0) {}
-		Tokeniser(const std::basic_string<CharType> &&str, const CharType delimiter)
+		Tokeniser(std::basic_string<CharType> &&str, const CharType delimiter)
 			: m_inputCopy(str), m_str(m_inputCopy), m_delimiter(delimiter), m_begin(0) {}
 
 		bool hasNext() const throw() {return m_begin != std::basic_string<CharType>::npos;}
 		std::basic_string<CharType> next();
+		void next(CharType *&start, CharType *&end);
 	private:
 		/* m_inputCopy is used only with temporary strings because they die early.
 		   In other cases no copying of the input string is used. */
-		const std::basic_string<CharType> m_inputCopy;
-		const std::basic_string<CharType> &m_str;
+		std::basic_string<CharType> m_inputCopy;
+		std::basic_string<CharType> &m_str;
 		const CharType m_delimiter;
-		size_t m_begin;
+		std::size_t m_begin;
 	};
 }
 
 template<typename CharType>
 std::basic_string<CharType> afc::Tokeniser<CharType>::next()
 {
+#ifdef AFC_EXCEPTIONS_ENABLED
 	if (m_begin == std::basic_string<CharType>::npos) {
 		throwException<IllegalStateException>("Tokeniser::next is called when it has not more tokens");
 	}
+#endif
+
 	const size_t end = m_str.find(m_delimiter, m_begin);
 	if (end == std::basic_string<CharType>::npos) {
 		auto token = m_str.substr(m_begin);
@@ -64,6 +68,26 @@ std::basic_string<CharType> afc::Tokeniser<CharType>::next()
 		auto token = m_str.substr(m_begin, end - m_begin);
 		m_begin = end + 1;
 		return token;
+	}
+}
+
+template<typename CharType>
+void afc::Tokeniser<CharType>::next(CharType *&start, CharType *&end)
+{
+#ifdef AFC_EXCEPTIONS_ENABLED
+	if (m_begin == std::basic_string<CharType>::npos) {
+		throwException<IllegalStateException>("Tokeniser::next is called when it has not more tokens");
+	}
+#endif
+
+	start = &m_str[m_begin];
+	const std::size_t endIdx = m_str.find(m_delimiter, m_begin);
+	if (endIdx == std::basic_string<CharType>::npos) {
+		end = &m_str[0] + m_str.size();
+		m_begin = std::basic_string<CharType>::npos;
+	} else {
+		end = &m_str[endIdx];
+		m_begin = endIdx + 1;
 	}
 }
 
