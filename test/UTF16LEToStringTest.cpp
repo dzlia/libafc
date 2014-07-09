953e0088cb9c64315fcab799e90966e035fa11d5
Dźmitry Laŭčuk <>
2014-07-09 13:42:55 +0300

    afc::UTF16LEToStringTest is migrated from libcpptest to libcppunit.

--- test/UTF16LEToStringTest.cpp
+++ test/UTF16LEToStringTest.cpp
@@ -1,3 +1,18 @@
+/* libafc - utils to facilitate C++ development.
+Copyright (C) 2013-2014 Dźmitry Laŭčuk
+
+libafc is free software: you can redistribute it and/or modify it under
+the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 #include "UTF16LEToStringTest.h"
 #include <afc/utils.h>
 #include <string>
@@ -5,54 +20,44 @@
 using namespace std;
 using namespace afc;
 
-afc::UTF16LEToStringTest::UTF16LEToStringTest()
-{
-	TEST_ADD(UTF16LEToStringTest::testEmptyString_toASCII);
-	TEST_ADD(UTF16LEToStringTest::testNonEmptyString_toASCII);
-	TEST_ADD(UTF16LEToStringTest::testMultiByteNonEmptyString_toASCII_withTransliteration);
-	TEST_ADD(UTF16LEToStringTest::testEmptyString_toUTF8);
-	TEST_ADD(UTF16LEToStringTest::testSimpleNonEmptyString_toUTF8);
-	TEST_ADD(UTF16LEToStringTest::testMultiByteNonEmptyString_toUTF8);
-}
-
 void afc::UTF16LEToStringTest::testEmptyString_toASCII()
 {
 	string s(utf16leToString(u16string(), "ascii"));
 
-	TEST_ASSERT_MSG(s == "", s.c_str());
+	CPPUNIT_ASSERT_EQUAL_MESSAGE(s.c_str(), string(""), s);
 }
 
 void afc::UTF16LEToStringTest::testNonEmptyString_toASCII()
 {
 	string s(utf16leToString(u16string(u"Hello, World!"), "ascii"));
 
-	TEST_ASSERT_MSG(s == "Hello, World!", s.c_str());
+	CPPUNIT_ASSERT_EQUAL_MESSAGE(s.c_str(), string("Hello, World!"), s);
 }
 
 void afc::UTF16LEToStringTest::testMultiByteNonEmptyString_toASCII_withTransliteration()
 {
 	string s(utf16leToString(u16string(u"\u0160\u010da\u015bcie"), "ascii//TRANSLIT"));
 
-	TEST_ASSERT_MSG(s == u8"??a?cie", s.c_str());
+	CPPUNIT_ASSERT_EQUAL_MESSAGE(s.c_str(), string(u8"??a?cie"), s);
 }
 
 void afc::UTF16LEToStringTest::testEmptyString_toUTF8()
 {
 	string s(utf16leToString(u16string(), "UTF-8"));
 
-	TEST_ASSERT_MSG(s == u8"", s.c_str());
+	CPPUNIT_ASSERT_EQUAL_MESSAGE(s.c_str(), string(u8""), s);
 }
 
 void afc::UTF16LEToStringTest::testSimpleNonEmptyString_toUTF8()
 {
 	string s(utf16leToString(u16string(u"Hello, World!"), "UTF-8"));
 
-	TEST_ASSERT_MSG(s == u8"Hello, World!", s.c_str());
+	CPPUNIT_ASSERT_EQUAL_MESSAGE(s.c_str(), string(u8"Hello, World!"), s);
 }
 
 void afc::UTF16LEToStringTest::testMultiByteNonEmptyString_toUTF8()
 {
 	string s(utf16leToString(u16string(u"\u0160\u010da\u015bcie"), "UTF-8"));
 
-	TEST_ASSERT_MSG(s == u8"\u0160\u010da\u015bcie", s.c_str());
+	CPPUNIT_ASSERT_EQUAL_MESSAGE(s.c_str(), string(u8"\u0160\u010da\u015bcie"), s);
 }
--- test/run_tests.cpp
+++ test/run_tests.cpp
@@ -1,5 +1,4 @@
 #include <cpptest.h>
-#include "UTF16LEToStringTest.h"
 #include "cpu/Int32Test.h"
 
 using namespace std;
@@ -9,8 +8,6 @@ using namespace Test;
 int main() {
 	TextOutput out(TextOutput::Verbose);
 	Suite allTests;
-	allTests.add(auto_ptr<Suite>(new UTF16LEToStringTest));
-
 	allTests.add(auto_ptr<Suite>(new Int32Test));
 	allTests.run(out);
 	return 0;
