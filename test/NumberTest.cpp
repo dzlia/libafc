42ca4a2e2a96790c165e6de5a6a970d4f5379a3e
Dźmitry Laŭčuk <>
2014-07-09 12:43:27 +0300

    afc::NumberTest is migrated from libcpptest to libcppunit.

--- test/NumberTest.cpp
+++ test/NumberTest.cpp
@@ -1,61 +1,71 @@
+/* libafc - utils to facilitate C++ development.
+Copyright (C) 2010-2014 Dźmitry Laŭčuk
+
+libafc is free software: you can redistribute it and/or modify it under
+the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 #include "NumberTest.h"
 #include <afc/number.h>
 
 using namespace std;
-
-afc::NumberTest::NumberTest()
-{
-	TEST_ADD(NumberTest::testAppendToStringInt);
-}
 
 void afc::NumberTest::testAppendToStringInt()
 {
 	{
 		string result;
 		appendToString<int>(123, 10, result);
-		TEST_ASSERT(result == "123");
+		CPPUNIT_ASSERT_EQUAL(string("123"), result);
 		appendToString<int>(-456, 10, result);
-		TEST_ASSERT(result == "123-456");
+		CPPUNIT_ASSERT_EQUAL(string("123-456"), result);
 	}
 	{
 		string result;
 		appendToString<int>(123, result);
-		TEST_ASSERT(result == "123");
+		CPPUNIT_ASSERT_EQUAL(string("123"), result);
 		appendToString<int>(-456, result);
-		TEST_ASSERT(result == "123-456");
+		CPPUNIT_ASSERT_EQUAL(string("123-456"), result);
 		appendToString<int>(0, result);
-		TEST_ASSERT(result == "123-4560");
+		CPPUNIT_ASSERT_EQUAL(string("123-4560"), result);
 	}
 	{
 		string result("aaa");
 		appendToString<int>(411787, 10, result);
-		TEST_ASSERT(result == "aaa411787");
+		CPPUNIT_ASSERT_EQUAL(string("aaa411787"), result);
 		appendToString<int>(411787, 27, result);
-		TEST_ASSERT(result == "aaa411787kona");
+		CPPUNIT_ASSERT_EQUAL(string("aaa411787kona"), result);
 		appendToString<int>(45896, 36, result);
-		TEST_ASSERT(result == "aaa411787konazew");
+		CPPUNIT_ASSERT_EQUAL(string("aaa411787konazew"), result);
 		appendToString<int>(45896, 35, result);
-		TEST_ASSERT(result == "aaa411787konazew12gb");
+		CPPUNIT_ASSERT_EQUAL(string("aaa411787konazew12gb"), result);
 	}
 	{
 		string result("aaa");
-		TEST_THROWS(appendToString<int>(234, 1, result), InvalidArgumentException &);
-		TEST_ASSERT(result == "aaa");
-		TEST_THROWS(appendToString<int>(234, 0, result), InvalidArgumentException &);
-		TEST_ASSERT(result == "aaa");
-		TEST_THROWS(appendToString<int>(234, 37, result), InvalidArgumentException &);
-		TEST_ASSERT(result == "aaa");
-		TEST_THROWS(appendToString<int>(234, 255, result), InvalidArgumentException &);
-		TEST_ASSERT(result == "aaa");
+		CPPUNIT_ASSERT_THROW(appendToString<int>(234, 1, result), InvalidArgumentException);
+		CPPUNIT_ASSERT_EQUAL(string("aaa"), result);
+		CPPUNIT_ASSERT_THROW(appendToString<int>(234, 0, result), InvalidArgumentException);
+		CPPUNIT_ASSERT_EQUAL(string("aaa"), result);
+		CPPUNIT_ASSERT_THROW(appendToString<int>(234, 37, result), InvalidArgumentException);
+		CPPUNIT_ASSERT_EQUAL(string("aaa"), result);
+		CPPUNIT_ASSERT_THROW(appendToString<int>(234, 255, result), InvalidArgumentException);
+		CPPUNIT_ASSERT_EQUAL(string("aaa"), result);
 	}
 	{
 		string result;
 		appendToString<int>(static_cast<int>(-0x80000000), 2, result);
-		TEST_ASSERT(result == "-10000000000000000000000000000000");
+		CPPUNIT_ASSERT_EQUAL(string("-10000000000000000000000000000000"), result);
 	}
 	{
 		string result;
 		appendToString(-20, result);
-		TEST_ASSERT(result == "-20");
+		CPPUNIT_ASSERT_EQUAL(string("-20"), result);
 	}
 }
--- test/run_tests.cpp
+++ test/run_tests.cpp
@@ -1,5 +1,4 @@
 #include <cpptest.h>
-#include "NumberTest.h"
 #include "RepositoryTest.h"
 #include "TokeniserTest.h"
 #include "UTF16LEToStringTest.h"
@@ -13,7 +12,6 @@ using namespace Test;
 int main() {
 	TextOutput out(TextOutput::Verbose);
 	Suite allTests;
-	allTests.add(auto_ptr<Suite>(new NumberTest));
 	allTests.add(auto_ptr<Suite>(new RepositoryTest));
 	allTests.add(auto_ptr<Suite>(new StringUtilTest));
 	allTests.add(auto_ptr<Suite>(new TokeniserTest));
