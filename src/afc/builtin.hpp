75b008ef566d9d96ae95903605af9b7b16fd91ff
Dźmitry Laŭčuk <>
2016-03-12 01:09:28 +0300

    Conditions of likely()/unlikely() are cast to bool to avoid ambiguity.

--- src/afc/builtin.hpp
+++ src/afc/builtin.hpp
@@ -1,5 +1,5 @@
 /* libafc - utils to facilitate C++ development.
-Copyright (C) 2014 Dźmitry Laŭčuk
+Copyright (C) 2014-2016 Dźmitry Laŭčuk
 
 libafc is free software: you can redistribute it and/or modify it under
 the terms of the GNU Lesser General Public License as published by
@@ -17,8 +17,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 #define AFC_BUILTIN_HPP_
 
 #ifdef __GNUG__
-	#define likely(x) __builtin_expect((x), true)
-	#define unlikely(x) __builtin_expect((x), false)
+	#define likely(x) __builtin_expect(static_cast<bool>(x), true)
+	#define unlikely(x) __builtin_expect(static_cast<bool>(x), false)
 #else
 	#define likely(x) (x)
 	#define unlikely(x) (x)
